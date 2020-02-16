99b34e049d6937f43d7870ab7adb7a75e5e403cb
Adrian So <adrian@sld.co.nz>
2020-02-17 10:52:11 +1300

    Update to 1.0.6

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
+# @adrianso/react-native-barcode-builder
 
-# react-native-barcode-builder
+React Native component to generate barcodes.
 
-React Native component to generate barcodes. Uses [JsBarcode](https://github.com/lindell/JsBarcode) for encoding of data.
+Uses [JsBarcode](https://github.com/lindell/JsBarcode) for encoding of data.
+
+Uses [@react-native-community/react-native-svg](https://github.com/react-native-community/react-native-svg) instead of [@react-native-community/art](https://github.com/react-native-community/art).
 
 ## Getting started
 
-This library uses [@react-native-community/react-native-svg](https://github.com/react-native-community/react-native-svg) instead of [@react-native-community/art](https://github.com/react-native-community/art)
-
 ### Step 1
 
-Install `react-native-barcode-builder`:
+Install `react-native-barcode-builder` and dependencies:
 
-    npm install react-native-barcode-builder --save
+    npm install react-native-barcode-builder react-native-svg
 
 or
 
-    yarn add react-native-barcode-builder
-  
+    yarn add react-native-barcode-builder react-native-svg
+
 ### Step 2
 
 Start using the component
@@ -24,7 +25,7 @@ Start using the component
 ```javascript
 import Barcode from 'react-native-barcode-builder';
 
-<Barcode value="Hello World" format="CODE128" />
+<Barcode value="Hello World" format="CODE128" />;
 ```
 
 You can find more info about the supported barcodes in the [JsBarcode README](https://github.com/lindell/JsBarcode#supported-barcodes).
--- index.js
+++ index.js
@@ -1,173 +1,119 @@
-import React, { PureComponent } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import PropTypes from 'prop-types';
-import Svg, {
-  Path,
-} from 'react-native-svg';
+// @flow
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
 
 import barcodes from 'jsbarcode/src/barcodes';
 
-export default class Barcode extends PureComponent {
-  static propTypes = {
-    /* what the barCode stands for */
-    value: PropTypes.string,
-    /* Select which barcode type to use */
-    format: PropTypes.oneOf(Object.keys(barcodes)),
-    /* Set the color of the bars */
-    lineColor: PropTypes.string,
-    /* Handle error for invalid barcode of selected format */
-    onError: PropTypes.func
-  };
-
-  static defaultProps = {
-    value: undefined,
-    format: 'CODE128',
-    text: undefined,
-    lineColor: '#000000',
-    onError: undefined
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      bars: [],
-      barCodeWidth: 0
-    };
+// encode() handles the Encoder call and builds the binary string to be rendered
+const encode = (text, Encoder, options) => {
+  // If text is not a non-empty string, throw error.
+  if (typeof text !== 'string' || text.length === 0) {
+    throw new Error('Barcode value must be a non-empty string');
   }
 
-  componentWillUpdate(nextProps) {
-    if (nextProps.value !== this.props.value) {
-      this.update(nextProps);
+  var encoder;
+
+  try {
+    encoder = new Encoder(text, options);
+  } catch (error) {
+    // If the encoder could not be instantiated, throw error.
+    throw new Error('Invalid barcode format.');
+  }
+
+  // If the input is not valid for the encoder, throw error.
+  if (!encoder.valid()) {
+    throw new Error('Invalid barcode for selected format.');
+  }
+
+  // Make a request for the binary data (and other infromation) that should be rendered
+  // encoded stucture is {
+  //  text: 'xxxxx',
+  //  data: '110100100001....'
+  // }
+  var encoded = encoder.encode();
+  return encoded;
+};
+
+const drawSvgBarCode = encoding => {
+  const rects = [];
+  // binary data of barcode
+  const binary = encoding.data;
+
+  let barWidth = 0;
+  let x = 0;
+  let yFrom = 0;
+
+  for (let b = 0; b < binary.length; b++) {
+    x = b;
+    if (binary[b] === '1') {
+      barWidth++;
+    } else if (barWidth > 0) {
+      rects[rects.length] = drawRect(x - barWidth, yFrom, barWidth, 100);
+      barWidth = 0;
     }
   }
 
-  componentDidMount() {
-    this.update();
+  // Last draw is needed since the barcode ends with 1
+  if (barWidth > 0) {
+    rects[rects.length] = drawRect(x - barWidth + 1, yFrom, barWidth, 100);
   }
 
-  componentDidUpdate() {
-    this.update();
-  }
+  return rects;
+};
 
-  update() {
-    const encoder = barcodes[this.props.format];
-    const encoded = this.encode(this.props.value, encoder, this.props);
+const drawRect = (x, y, width, height) => {
+  return `M${x},${y}h${width}v${height}h-${width}z`;
+};
+
+type Props = {|
+  +style: Object,
+  +value: string,
+  +format: string,
+  +lineColor: string,
+  +flat: boolean,
+  +onError: Function,
+|};
+
+const Barcode = (props: Props) => {
+  const { format, value, lineColor, flat } = props;
+  const { style, ...barcodeProps } = props;
+  const [bars, setBars] = useState([]);
+  const [barCodeWidth, setBarCodeWidth] = useState(0);
+
+  useEffect(() => {
+    const encoder = barcodes[format];
+    const encoded = encode(value, encoder, barcodeProps);
 
     if (encoded) {
-      this.state.bars = this.drawSvgBarCode(encoded, this.props);
-      this.state.barCodeWidth = encoded.data.length;
+      setBars(drawSvgBarCode(encoded));
+      setBarCodeWidth(encoded.data.length);
     }
-  }
+  }, [format, value, lineColor, flat]);
 
-  drawSvgBarCode(encoding) {
-    const rects = [];
-    // binary data of barcode
-    const binary = encoding.data;
+  return (
+    <View style={style}>
+      <Svg
+        height="100%"
+        width="100%"
+        viewBox={`0 0 ${barCodeWidth} 100`}
+        preserveAspectRatio="xMinYMin slice"
+        fill={lineColor}
+      >
+        <Path d={bars.join(' ')} />
+      </Svg>
+    </View>
+  );
+};
 
-    let barWidth = 0;
-    let x = 0;
-    let yFrom = 0;
-    // alert(JSON.stringify(options));
+export default Barcode;
 
-    for (let b = 0; b < binary.length; b++) {
-      x = b;
-      if (binary[b] === '1') {
-        barWidth++;
-      } else if (barWidth > 0) {
-        rects[rects.length] = this.drawRect(
-          x - barWidth,
-          yFrom,
-          barWidth,
-          100
-        );
-        barWidth = 0;
-      }
-    }
-
-    // Last draw is needed since the barcode ends with 1
-    if (barWidth > 0) {
-      rects[rects.length] = this.drawRect(
-        x - barWidth + 1,
-        yFrom,
-        barWidth,
-        100
-      );
-    }
-
-    return rects;
-  }
-
-  drawRect(x, y, width, height) {
-    return `M${x},${y}h${width}v${height}h-${width}z`;
-  }
-
-  getTotalWidthOfEncodings(encodings) {
-    let totalWidth = 0;
-    for (let i = 0; i < encodings.length; i++) {
-      totalWidth += encodings[i].width;
-    }
-    return totalWidth;
-  }
-
-  // encode() handles the Encoder call and builds the binary string to be rendered
-  encode(text, Encoder, options) {
-    // If text is not a non-empty string, throw error.
-    if (typeof text !== "string" || text.length === 0) {
-      if (this.props.onError) {
-        this.props.onError(new Error('Barcode value must be a non-empty string'));
-        return;
-      }
-      throw new Error('Barcode value must be a non-empty string');
-    }
-
-    var encoder;
-
-    try {
-      encoder = new Encoder(text, options);
-    } catch (error) {
-      // If the encoder could not be instantiated, throw error.
-      if (this.props.onError)  {
-        this.props.onError(new Error('Invalid barcode format.'));
-        return;
-      }
-      throw new Error('Invalid barcode format.');
-    }
-
-    // If the input is not valid for the encoder, throw error.
-    if (!encoder.valid()) {
-      if (this.props.onError) {
-        this.props.onError(new Error('Invalid barcode for selected format.'));
-        return;
-      }
-      throw new Error('Invalid barcode for selected format.');
-    }
-
-    // Make a request for the binary data (and other infromation) that should be rendered
-    // encoded stucture is {
-    //  text: 'xxxxx',
-    //  data: '110100100001....'
-    // }
-    var encoded = encoder.encode();
-
-    return encoded;
-  }
-
-  render() {
-    this.update();
-    return (
-      <View style={{width: "100%", height: "100%"}}>
-        <Svg
-          height="100%"
-          width="100%"
-          viewBox={`0 0 ${this.state.barCodeWidth} 100`}
-          // width={200}
-          preserveAspectRatio="xMinYMin slice"
-          fill={this.props.lineColor}>
-          <Path
-            d={this.state.bars.join(' ')}
-          />
-        </Svg>
-      </View>
-    );
-  }
-}
+Barcode.defaultProps = {
+  style: {},
+  value: undefined,
+  format: 'CODE128',
+  text: undefined,
+  lineColor: '#000000',
+  flat: false,
+  onError: () => {},
+};
